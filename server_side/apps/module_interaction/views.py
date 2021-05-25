074eefd96c35c15f59a28995fc378fd1a7ca3dda
ArchibaldHof <artik.kh@gmail.com>
2021-05-25 14:20:27 +0300

    update commit

--- server_side/apps/module_interaction/views.py
+++ server_side/apps/module_interaction/views.py
@@ -16,25 +16,25 @@ def webhook(request):
     text = utils.get_string('text', request_t['originalDetectIntentRequest']['payload']['data'])
     text_q = utils.get_string('queryText', request_t['queryResult'])
     if text == '/start':
-        USER_LANGUAGE = request_t['originalDetectIntentRequest']['payload']['data']['from']['language_code']
+        settings.LANGUAGE_CODE = request_t['originalDetectIntentRequest']['payload']['data']['from']['language_code']
     reply = '{}'
     if text == '/start':
-        reply = send_welcome(USER_LANGUAGE, settings.LANGUAGE_CODE)
+        reply = send_welcome(settings.LANGUAGE_CODE, USER_LANGUAGE)
     if text == '/consult':
-        reply = send_consult(USER_LANGUAGE, settings.LANGUAGE_CODE)
+        reply = send_consult(settings.LANGUAGE_CODE, USER_LANGUAGE)
     if text == '/help':
-        reply = send_help(USER_LANGUAGE, settings.LANGUAGE_CODE)
+        reply = send_help(settings.LANGUAGE_CODE, USER_LANGUAGE)
     if text_q == 'change_language':
-        reply = change_language(USER_LANGUAGE, settings.LANGUAGE_CODE)
+        reply = change_language(settings.LANGUAGE_CODE, USER_LANGUAGE)
     if text_q in ('en', 'de', 'ru', 'be', 'uk'):
-        USER_LANGUAGE = text_q
-        reply = send_welcome(text_q, settings.LANGUAGE_CODE)
+        settings.LANGUAGE_CODE = text_q
+        reply = send_welcome(settings.LANGUAGE_CODE, USER_LANGUAGE)
     if text_q == 'start_conversation':
-        reply = start_conversation(USER_LANGUAGE, settings.LANGUAGE_CODE)
+        reply = start_conversation(settings.LANGUAGE_CODE, USER_LANGUAGE)
     if text_q == 'archive':
-        reply = send_wiki(USER_LANGUAGE, settings.LANGUAGE_CODE)
+        reply = send_wiki(settings.LANGUAGE_CODE, USER_LANGUAGE)
     if text_q == 'video':
-        reply = send_youtube(USER_LANGUAGE, settings.LANGUAGE_CODE)
+        reply = send_youtube(settings.LANGUAGE_CODE, USER_LANGUAGE)
     if text_q == 'article':
-        reply = send_article(USER_LANGUAGE, settings.LANGUAGE_CODE)
+        reply = send_article(settings.LANGUAGE_CODE, USER_LANGUAGE)
     return JsonResponse(reply, safe=False)
